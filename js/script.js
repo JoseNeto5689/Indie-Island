d5d297e7463748b07da2bb17777234385d7c6dd6
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-01-27 14:46:31 -0300

    Menu hamburguer atualizado

--- js/script.js
+++ js/script.js
@@ -2,14 +2,17 @@ const menu = document.querySelector("div#menu")
 const modal = document.querySelector("div#modal")
 const backLink = document.querySelector("a h3#backToPage") 
 const stores = document.querySelector("#stores")
+const body = document.querySelector("body")
 
 function openModal(){
     modal.style.display = "flex"
     menu.style.zIndex = -1
+    body.style.overflowY = "hidden";
 }
 function closeModal(){
     modal.style.display = "none"
     menu.style.zIndex = 1
+    body.style.overflowY = "auto";
 }
 
 function navigateToStores(){
