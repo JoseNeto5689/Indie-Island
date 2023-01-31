b2a5d5b3de1fddd34aebffd3020c96087c1041df
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-01-31 15:41:02 -0300

    Página de game dedicada para cuphead criada

--- js/expandImage.js
+++ js/expandImage.js
@@ -2,14 +2,17 @@ const images = document.querySelectorAll(".game-gallery")
 const closeImage = document.querySelector("#closeImage")
 const modalImage = document.querySelector(".modal-image")
 const image = document.querySelector("#image")
+const scroll = document.querySelector("body")
 
 closeImage.addEventListener("click", (event) => {
     modalImage.style.display = "none"
+    scroll.style.overflowY = "auto"
 })
 
 images.forEach((item) => {
     item.addEventListener("click", (event) => {
         image.src = event.target.src
         modalImage.style.display = "flex"
+        scroll.style.overflowY = "hidden"
     })
 })
